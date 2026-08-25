0f6258cbc6952eae8b8b34546470e12cd7e5b591
SteffRhes <steffrhes@pm.me>
2026-08-25 20:08:33 +0200

    test_individual.py: add

---
+++ tests/test_individual.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from importlib import util
+from importlib.machinery import SourceFileLoader
+from pathlib import Path
+
+
+apptainer_main_path = Path(__file__).resolve().parent.parent / "apptainer-compose"
+spec = util.spec_from_file_location("apptainer_compose", apptainer_main_path, loader=SourceFileLoader("apptainer_compose", str(apptainer_main_path)))
+apptainer_compose = util.module_from_spec(spec)
+spec.loader.exec_module(apptainer_compose)
+
+
+sys.argv = None
+os.chdir("./test_cases/compose_yaml/execution/services_service_command/")
+sys.argv = ["../apptainer-compose", "--verbose", "up"]
+apptainer_compose.main()
